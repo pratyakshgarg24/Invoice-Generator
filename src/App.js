5fcdad1b068924d33f3eca1f54e5b2e15c8e5de8
Pratyaksh Garg <pgpratyaksh24@gmail.com>
2024-04-26 09:20:52 +0530

    Third Commit 26/04/2024

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import InvoiceForm from './components/InvoiceForm';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100" style={{ backgroundColor: '#D6EBF2' }}>
       <div className="mx-auto max-w-7xl">
+      <h1 className="app-heading">Invoice Generator</h1>
         <InvoiceForm />
       </div>
     </div>
